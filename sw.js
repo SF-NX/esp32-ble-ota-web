2b9bed171ea57b590c23fb70fa98e7bcca2e5708
sfnx <sfnx233@gmail.com>
2026-07-16 00:18:03 +0800

    放大并对齐蓝牙状态图标

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bleota-web-v19-platform-status-icon';
+const CACHE = 'bleota-web-v20-platform-status-icon';
 const ASSETS = ['./', './index.html', './tutorial.html', './tutorial.css', './styles.css', './app.js', './protocol.js', './manifest.webmanifest', './icons/app-icon.svg'];
 
 self.addEventListener('install', (event) => {
